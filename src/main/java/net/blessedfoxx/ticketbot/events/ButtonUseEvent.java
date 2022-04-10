9276a41cce44f02228f2190f99f6d4d8813511bf
BlessedFoxX <85083169+BlessedFoxX@users.noreply.github.com>
2022-04-10 15:30:58 +0200

    Added Ticket Banned Function

--- src/main/java/net/blessedfoxx/ticketbot/events/ButtonUseEvent.java
+++ src/main/java/net/blessedfoxx/ticketbot/events/ButtonUseEvent.java
@@ -1,5 +1,6 @@
 package net.blessedfoxx.ticketbot.events;
 
+import net.blessedfoxx.ticketbot.utils.Ticket;
 import net.blessedfoxx.ticketbot.utils.TicketManager;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
@@ -15,15 +16,21 @@ public class ButtonUseEvent extends ListenerAdapter {
         Member member = event.getMember();
         Button button = event.getButton();
 
+        if (button.getId() !=null)
         if (button.getId().equalsIgnoreCase("claim")) {
             TicketManager.claimByButtonTicket(member, event);
         }else if (button.getId().equalsIgnoreCase("close")) {
             event.deferReply(true).setContent(":x: Ticket Closed!").queue();
             TicketManager.deleteTicket(event.getTextChannel());
         }else if (button.getId().equalsIgnoreCase("create")) {
-            if (member !=null)
-                TicketManager.createTicket(member);
-                event.getInteraction().deferReply(true).setContent(":white_check_mark: Your ticket " + TicketManager.getTicketChannel().getAsMention() +" has been created!").queue();
+
+            if (member != null)
+                if (TicketManager.isTicketBanned(member)) {
+                    TicketManager.createTicket(member);
+                    event.getInteraction().deferReply(true).setContent(":white_check_mark: Your ticket " + TicketManager.getTicketChannel().getAsMention() + " has been created!").queue();
+                }else{
+                    event.getInteraction().deferReply(true).setContent(Ticket.getTicketBanned()).queue();
+                }
         }
     }
 }
